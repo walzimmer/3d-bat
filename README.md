5d191686a41c9f219d972008163e126bdbb3a592
Walter Zimmer <wzimmer@eng.ucsd.edu>
2018-10-18 13:58:29 -0700

    added download button
    Former-commit-id: bed22d70059f4133068ec74ca4701f5f265bef4e

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 3D_BoundingBox_Annotation_Tool_3D_BAT
+# Instructions
+1. Step: draw bounding box in the camera image
+2. Step: choose current bounding box by activating it
+3. Step: You can move it in image space or even change its size by drag and droping
+4. Step: Switch into PCD MODE into birds-eye-view
+5. Step: Place 3D label into 3D scene to corresponding 2D label
+6. Step: Adjust label: 
+    1. drag and dropping directly on label to change position or size
+    2. use control bar to change position and size (horizontal bar -> rough adjustment, vertical bar -> fine adjustment)
+    3. Go into camera view to check label with higher intensity and bigger point size
+7. Step: Choose label from drop down list
+8. Step: Repeat steps 1-7 for all objects in the scene
+9. Step: Save labels into file
+10. Step: Click on 'HOLD' button if you want to keep the same label positions and sizes 
+11. Step: click on 'Next camera image' 
+
+                        
+
--- js/util/ajax_wrapper.js
+++ js/util/ajax_wrapper.js
@@ -4,54 +4,66 @@
 
 var __labelData = []
 
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 function request(options) {
     if (options.type == "GET") {
-	switch (options.url) {
-	    case "/labels/":
-		var responseDict = {
-		    blob: "input",
-		    progress: 102
-		};
-		var res = {responseText: JSON.stringify([responseDict])};
-		options.complete(res);
-		break;
-	    case "/label/image_size/":
-		var responseDict = {
-		    width: 800,
-		    height: 600
-		};
-		var res = {responseText: JSON.stringify(responseDict)};
-		options.complete(res);
-		break;
-	    case "/label/file_names/":
-		var responseDict = {
-		    file_names: ["000000", "000001","000002"]
-		};
-		var res = {responseText: JSON.stringify(responseDict)};
-		options.complete(res);
-		break;
-	    case "/label/annotations/":
-		var fileName = options.data["file_name"];
-		var res;
-		if (fileName in __labelData) {
-		    res = JSON.parse(__labelData[fileName]);
-		} else {
-		    res = parseAnnotationFile(fileName);
-		}
-		options.success(res);
-		break;
-	}
+        switch (options.url) {
+            case "/labels/":
+                var responseDict = {
+                    blob: "input",
+                    progress: 102
+                };
+                var res = {responseText: JSON.stringify([responseDict])};
+                options.complete(res);
+                break;
+            case "/label/image_size/":
+                var responseDict = {
+                    width: 800,
+                    height: 600
+                };
+                var res = {responseText: JSON.stringify(responseDict)};
+                options.complete(res);
+                break;
+            case "/label/file_names/":
+                numFiles = 3962;
+                fileNameArray = [];
+                for (var i = 0; i < numFiles; i++) {
+                    fileNameArray.push(pad(i, 6))
+                }
+                var responseDict = {
+                    file_names: fileNameArray
+                };
+                var res = {responseText: JSON.stringify(responseDict)};
+                options.complete(res);
+                break;
+            case "/label/annotations/":
+                var fileName = options.data["file_name"];
+                var res;
+                if (fileName in __labelData) {
+                    res = JSON.parse(__labelData[fileName]);
+                } else {
+                    res = parseAnnotationFile(fileName);
+                }
+                options.success(res);
+                break;
+        }
     } else if (options.type == "POST") {
-	switch (options.url) {
-	    case "/label/annotations/":
-		/* if (options.data["label_id"] == 2) {*/
-		__labelData[options.data["file_name"]] = options.data["annotations"];
-		options.success("None");
-		/* } else {
-		   options.error();
-		   }*/
-		break;
-	}
+        switch (options.url) {
+            case "/label/annotations/":
+                /* if (options.data["label_id"] == 2) {*/
+                __labelData[options.data["file_name"]] = options.data["annotations"];
+                options.success("None");
+                /* } else {
+                   options.error();
+                   }*/
+                break;
+        }
     }
 };
 
@@ -59,33 +71,35 @@ function parseAnnotationFile(fileName) {
     var rawFile = new XMLHttpRequest();
     var res = [];
     rawFile.open("GET", labelTool.workBlob + '/Annotations/' + fileName, false);
-    rawFile.onreadystatechange = function (){
-	if(rawFile.readyState === 4){
-	    if(rawFile.status === 200 || rawFile.status == 0) {
-		var allText = rawFile.responseText;
-		var str_list = allText.split("\n");
-		for (var i = 0 ; i < str_list.length ; i++) {
-		    var str = str_list[i].split(" ");
-		    if(str.length == 16){
-			res.push({label: str[0],
-				  truncated: str[1],
-				  occluded: str[2],
-				  alpha: str[3],
-				  left: str[4],
-				  top: str[5],
-				  right: str[6],
-				  bottom: str[7],
-				  height: str[8],
-				  width: str[9],
-				  length:str[10],
-				  x: str[11],
-				  y: str[12],
-				  z: str[13],
-				  rotation_y: str[14]});
-		    }
-		}
-	    }
-	}
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                var str_list = allText.split("\n");
+                for (var i = 0; i < str_list.length; i++) {
+                    var str = str_list[i].split(" ");
+                    if (str.length == 16) {
+                        res.push({
+                            label: str[0],
+                            truncated: str[1],
+                            occluded: str[2],
+                            alpha: str[3],
+                            left: str[4],
+                            top: str[5],
+                            right: str[6],
+                            bottom: str[7],
+                            height: str[8],
+                            width: str[9],
+                            length: str[10],
+                            x: str[11],
+                            y: str[12],
+                            z: str[13],
+                            rotation_y: str[14]
+                        });
+                    }
+                }
+            }
+        }
     }
     rawFile.send(null);
     return res;
