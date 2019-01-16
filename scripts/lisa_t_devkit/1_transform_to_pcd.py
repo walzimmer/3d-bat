8634c735fcb8894c69a308c2a48bd6529c56b9fe
Walter Zimmer <wzimmer@eng.ucsd.edu>
2019-01-16 09:32:22 -0800

    added support to change sequences
    Former-commit-id: b12a50cd1b818cf4651e0c465a27e6cbc4aaffa2

--- scripts/lisa_t_devkit/1_transform_to_pcd.py
+++ scripts/lisa_t_devkit/1_transform_to_pcd.py
@@ -2,16 +2,20 @@ import os
 import random
 
 # sequence = '2018-05-23-001-frame-00042917-00043816'# DONE
-sequence = '2018-05-23-001-frame-00077323-00078222'
-path = '/media/cvrr/161d15ca-26dc-4d36-b085-945b15ce24b8/sandbox/3D_BoundingBox_Annotation_Tool_3D_BAT/input/LISA_T/' + sequence + '/pointclouds/'
+# sequence = '2018-05-23-001-frame-00077323-00078222'# DONE
+# sequence = '2018-05-23-001-frame-00080020-00080919'# DONE
+sequence = '2018-05-23-001-frame-00106993-00107892'
 
-for filename in sorted(os.listdir(path)):
-    with open(path + filename) as file_reader:
+path_in = '/media/cvrr/data/sandbox/datasets/lisat/data/sequences/' + sequence + '/undistorted/velodyne/'
+path_out = '/media/cvrr/data/sandbox/3D_BoundingBox_Annotation_Tool_3D_BAT/input/LISA_T/' + sequence + '/pointclouds/'
+
+for filename in sorted(os.listdir(path_in)):
+    with open(path_in + filename) as file_reader:
         lines = file_reader.readlines()
         numPoints = len(lines)
         header = '# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z intensity\nSIZE 4 4 4 4\nTYPE F F F F\nCOUNT 1 1 1 1\nWIDTH ' + str(
             numPoints) + '\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS ' + str(numPoints) + '\nDATA ascii\n'
-        with open(path + filename.replace('.txt', '.pcd'), 'w') as file_writer:
+        with open(path_out + filename.replace('.txt', '.pcd'), 'w') as file_writer:
             file_writer.write(header)
             for line in lines:
                 line = line.rstrip()
