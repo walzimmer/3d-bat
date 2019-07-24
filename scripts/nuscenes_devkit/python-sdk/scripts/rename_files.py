05b0cb9fd1e5ef5e700e3ad60dc392ecbc9677cc
Walter Zimmer <walterzimmer@gmx.de>
2019-07-24 12:49:06 +0200

    remove lisat data
    Former-commit-id: 2bfc33e21dfb06b86563ab7b77ab787f6577498c

--- scripts/nuscenes_devkit/python-sdk/scripts/rename_files.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = '/media/cvrr/161d15ca-26dc-4d36-b085-945b15ce24b8/sandbox/datasets/lisat/data/sequences/2018-05-23-001-frame-00042917-00043816/undistorted/CAM_FRONT/'
-i = 0
-for file in sorted(os.listdir(path)):
-    os.rename(path + file, path + str(i).zfill(6) + '.jpg')
-    i = i + 1
