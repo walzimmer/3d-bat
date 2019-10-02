39335766545b3c5fa37e0848622b2929ff20ebf5
Walter Zimmer <walterzimmer@gmx.de>
2019-10-02 13:10:15 +0200

    remove unused files and fix width of image panel
    Former-commit-id: bce5e93e3437226ac7139e3b656902b722047917

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import ReactDom from "react-dom";
-
-// what to render, where to render
-import * as ReactDOM from "react-dom";
-import React from "react";
-
-ReactDOM.render(<h1> Hello world !</h1>, document.getElementById('root'));
